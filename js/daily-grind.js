0294636db58e1e37ecd4ffbe78dce5fb75f47c7f
Selomea Beyene <selomea.beyene@example.com>
2024-11-20 12:42:47 -0800

    added all days of the week

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -7,8 +7,28 @@ switch(myDay){
         today = "Sunday";
     break;
 
+    case 1:
+        today = "Monday";
+    break;
+
     case 2:
         today = "Tuesday";
+    break;
+
+    case 3:
+        today = "Wednesday";
+    break;
+
+    case 4:
+        today = "Thursday";
+    break;
+
+    case 5:
+        today = "Friday";
+    break;
+
+    case 6:
+        today = "Saturday";
     break;
 
     default:
